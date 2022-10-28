516bd222e267540debd0c8afeef22f7f3dd3a135
Markus <mark394e@stud.kea.dk>
2022-10-28 18:50:20 +0200

    det var alt for idag 28/10

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import "./style.scss";
+
+const toggleBtn = document.querySelector(".toggle-btn");
+
+const navbarLinks = document.querySelector(".navbar-links");
+
+toggleBtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
