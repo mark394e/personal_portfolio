d0c9e5aed42f8553431dd33008f3aa99be15c494
Markus <mark394e@stud.kea.dk>
2022-11-01 12:57:37 +0100

    a bunch of styling n shit

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import "./style.scss";
+import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
+
+const progressbar = document.querySelector(".progressbar");
 
 const toggleBtn = document.querySelector(".toggle-btn");
 
@@ -7,3 +10,25 @@ const navbarLinks = document.querySelector(".navbar-links");
 toggleBtn.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
+
+// borrowed code from css-tricks.com
+const myScrollTimeline = new ScrollTimeline({
+  source: document.scrollingElement,
+  orientation: "block",
+  scrollOffsets: [new CSSUnitValue(0, "percent"), new CSSUnitValue(100, "percent")],
+});
+
+const properties2 = {
+  duration: 1,
+  fill: "forwards",
+  timeline: myScrollTimeline,
+};
+
+const keyframes2 = [
+  { backgroundColor: "#f7620c", transform: "scaleX(0)" },
+  { backgroundColor: "#f7620c", transform: "scaleX(1.001)" },
+];
+// ------------------------- end of borrowed code ------------------------------
+
+// adding animation to progressbar
+const progress = progressbar.animate(keyframes2, properties2);
