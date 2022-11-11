7bcc75d4c42d47d310d7b44cd331ff255d9215c6
Markus <mark394e@stud.kea.dk>
2022-11-11 17:10:40 +0100

    contact section og ny header

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import "./style.scss";
 import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
 
-const progressbar = document.querySelector(".progressbar");
-
 const toggleBtn = document.querySelector(".toggle-btn");
 
 const navbarLinks = document.querySelector(".navbar-links");
@@ -11,28 +9,7 @@ toggleBtn.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 
-// borrowed code from css-tricks.com
-const myScrollTimeline = new ScrollTimeline({
-  source: document.scrollingElement,
-  orientation: "block",
-  scrollOffsets: [new CSSUnitValue(0, "vw"), new CSSUnitValue(100, "vw")],
-});
-
-const properties2 = {
-  duration: 1,
-  fill: "forwards",
-  timeline: myScrollTimeline,
-};
-
-const keyframes2 = [
-  { backgroundColor: "#f7620c", transform: "scaleX(0)" },
-  { backgroundColor: "#f7620c", transform: "scaleX(1.001)" },
-];
-// ------------------------- end of borrowed code ------------------------------
-
-// adding animation to progressbar
-const progress = progressbar.animate(keyframes2, properties2);
-
+// ------------------------- image on hover effect ------------------------------
 let cursorImage = document.querySelector(".cursorImage");
 
 document.addEventListener("mousemove", function (e) {
