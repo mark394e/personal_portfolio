965caa13c0294afdb305de4a7b7d83ab83f7a1eb
Markus <mark394e@stud.kea.dk>
2022-11-11 13:48:19 +0100

    projects styled and animated

--- main.js
+++ main.js
@@ -32,3 +32,23 @@ const keyframes2 = [
 
 // adding animation to progressbar
 const progress = progressbar.animate(keyframes2, properties2);
+
+let cursorImage = document.querySelector(".cursorImage");
+
+document.addEventListener("mousemove", function (e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  cursorImage.style.left = x + "px";
+  cursorImage.style.top = y + "px";
+});
+
+document.querySelectorAll(".marquee").forEach((e, index) => {
+  e.addEventListener("mouseover", function (a) {
+    cursorImage.style.display = "block";
+    // cursorImage.style.backgroundImage = `url("https://picsum.photos/id/${index * 12}/300/260")`;
+    cursorImage.style.backgroundImage = `url(images/${e.dataset.image}.png)`;
+  });
+  e.addEventListener("mouseleave", () => {
+    cursorImage.style.backgroundImage = "none";
+  });
+});
