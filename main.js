f779e561737ab66f4a0583ab865f69c4bec94a52
Markus <mark394e@stud.kea.dk>
2022-11-12 13:28:41 +0100

    ny burgermenu og scroll pil

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 import "./style.scss";
 import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
 
-const toggleBtn = document.querySelector(".toggle-btn");
+// // const toggleBtn = document.querySelector(".toggle-btn");
 
+// const navbarLinks = document.querySelector(".navbar-links");
+
+// toggleBtn.addEventListener("click", () => {
+//   navbarLinks.classList.toggle("active");
+// });
+
+// ------------------------- burger menu ------------------------------
+const menuBtn = document.querySelector(".menu-btn");
 const navbarLinks = document.querySelector(".navbar-links");
+let menuOpen = false;
 
-toggleBtn.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    navbarLinks.classList.add("active");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    navbarLinks.classList.remove("active");
+    menuOpen = false;
+  }
+});
+
+document.querySelectorAll(".navbar-links a").forEach((a) => {
+  a.addEventListener("click", () => {
+    menuBtn.classList.remove("open");
+    navbarLinks.classList.remove("active");
+    menuOpen = false;
+  });
 });
 
 // ------------------------- image on hover effect ------------------------------
