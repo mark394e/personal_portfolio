b19c5210c2429200af6771a9c2bd57c6c9f184cf
Markus <mark394e@stud.kea.dk>
2022-11-15 09:22:23 +0100

    fixed hover img

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ document.querySelectorAll(".marquee").forEach((e, index) => {
   e.addEventListener("mouseover", function (a) {
     cursorImage.style.display = "block";
     // cursorImage.style.backgroundImage = `url("https://picsum.photos/id/${index * 12}/300/260")`;
-    cursorImage.style.backgroundImage = `url(images/${e.dataset.image}.png)`;
+    cursorImage.style.backgroundImage = `url(../images/${e.dataset.image}.png)`;
   });
   e.addEventListener("mouseleave", () => {
     cursorImage.style.backgroundImage = "none";
