de17b38892a327db063054309ac07ad49ee62da7
Markus <mark394e@stud.kea.dk>
2022-11-15 10:24:44 +0100

    fixing hover images

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ document.querySelectorAll(".marquee").forEach((e, index) => {
   e.addEventListener("mouseover", function (a) {
     cursorImage.style.display = "block";
     // cursorImage.style.backgroundImage = `url("https://picsum.photos/id/${index * 12}/300/260")`;
-    cursorImage.style.backgroundImage = `url(../images/${e.dataset.image}.png)`;
+    cursorImage.style.backgroundImage = `url(hoffmannlund.dk/images/${e.dataset.image}.png)`;
   });
   e.addEventListener("mouseleave", () => {
     cursorImage.style.backgroundImage = "none";
